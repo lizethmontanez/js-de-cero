857bdf2206b1087978b71bca4c3402fafe02ea5d
Lizeth <lizeth.mcx@gmail.com>
2025-02-04 20:23:33 -0600

    Comandos de consola

---
+++ tipos-de-datos.js
@@ -0,0 +1,41 @@
+//Este es un comentario en JS
+
+//console.log es una función que se utiliza para imprimir en consola
+console.log("Hola mundo");
+
+//Declaración de variable
+var nombre = "Lizeth"; //string
+var edad = 25; //number
+var casada = false; //bolean
+var vacunas = true; //boolean
+var hijos = null; //null
+var dato; //undefined
+
+//typeoff para saber el tipo de dato de una variable
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+//La semantica es importante
+var nombre = "Francia";
+var pais = 20;
+
+/*comentarios en bloque*/
+
+//Ejercicios de campus
+console.log(typeof(42));
+console.log(typeof("veinticinco"));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof("")); //APARECE ERROR DE SINTAXIS
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+console.log(typeof(10<2));
+console.log(typeof(hijos));
+console.log(typeof(10-2));
+console.log(typeof("Hola mundo"));
