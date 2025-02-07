1e755eee0783a85d8b565753f3b65eb5b0acd1f2
Lizeth <lizeth.mcx@gmail.com>
2025-02-06 18:59:32 -0600

    Teoria variables.js

---
+++ variables.js
@@ -0,0 +1,15 @@
+var nombre = "Lizeth"
+var nombre = "Liz"
+
+//Let no permite declarar variables,lo que significa que no puedes declarar la misma variable dos veces
+let apellido1 ="Montanez";
+let apellido2 ="Cervantes";
+
+//Se puede reasignar el valor de la variable así:, sin embargo el nombre de la variable no se cambia
+apellido1 = "Perez";
+console.log(apellido1);
+
+//cons es una constante, no se puede reasignar el valor
+const PI = 3.1416
+
+PI = 4;
